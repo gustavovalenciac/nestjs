4d6ea77f6c08ec6cd6e4057e8113f0c4452d2bee
Gustavo Valencia <gustavovalencia1998@hotmail.com>
2023-05-24 07:29:09 -0500

    test nestjs

---
+++ src/01-bases/01-types.ts
@@ -0,0 +1,10 @@
+export let name:string = 'Gustavo';
+export const age: number = 35;
+export const isValid:boolean = true;
+
+name = 'Melissa';
+
+export const templateString = ` Esto es un string
+multilinea ${name}`;
+
+console.log( templateString );
