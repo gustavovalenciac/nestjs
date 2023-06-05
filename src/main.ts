2e1cd4ab4143e602e1263d81ab64919c7de208ec
Gustavo <gustavovalencia1998@hotmail.com>
2023-06-05 15:38:44 -0500

    nest pruebas 05-06-23

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import { name } from './01-bases/01-types.ts';
+//import { name } from './01-bases/01-types.ts';
+import { charmander } from './01-bases/04-injection.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -12,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScrip + ${name}</h1>
+    <h1> ${charmander.name}, ${charmander.id} </h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
